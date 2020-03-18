f43f4e8a302326cbdf3c1b5d1485ee5e1a2a560d
Armando <josearmandoperez84@gmail.com>
2020-03-18 15:30:11 -0600

    vista dashBoard y componete posision del sol y implemetacion de  consumo de api del tiempo

---
+++ Scripts/JsViews/JsDashBoard.js
@@ -0,0 +1,55 @@
+﻿
+$(document).ready(function () {
+
+
+    //GetRelativeHumidity();
+    GetSolarPosition();
+
+    function GetSolarPosition() {
+
+        var times = SunCalc.getTimes(new Date(), 51.5, -0.1);
+        var sunrisePos = SunCalc.getPosition(new Date(), 23.6, -102.5)
+        var sunriseAzimuth = sunrisePos.azimuth * 180 / Math.PI;
+        console.log(sunriseAzimuth);
+        console.log(times);
+        $("#h3azimut").text(parseInt(sunriseAzimuth) + "°");
+        $("#lastazimut").text(moment().format('h:mm:ss a'));
+    }
+   
+
+    function GetRelativeHumidity() {
+     
+        var settings = {
+            'cache': false,
+            'dataType': "jsonp",
+            "async": true,
+            "crossDomain": true,
+            "url": "https://dataservice.accuweather.com/currentconditions/v1/242560?apikey=kTnd7AxjWopHBnCd7uGT6xhRGyxAVjHx&language=es-ES&details=true",
+            "method": "GET",
+            "headers": {
+                "accept": "application/json",
+                "Access-Control-Allow-Origin": "*"
+            }
+        }
+
+        $.ajax(settings).done(function (response) {
+
+            $("#relativeHumedityH3").text(response[0].RelativeHumidity);
+            $("#lastHumedity").text(moment().format('h:mm:ss a'));
+            console.log(response[0].RelativeHumidity);
+
+        });
+
+
+    }
+
+
+    window.setInterval(function () {
+        GetRelativeHumidity();
+    }, 16000);
+
+    window.setInterval(function () {
+        GetSolarPosition();
+    }, 25000);
+
+});
---
+++ Scripts/raspi-temp.js
@@ -0,0 +1,99 @@
+'use strict';
+
+var child_process = require('child_process');
+var path = require('path');
+var raspi_temp = exports;
+
+//-----------------------------------------------
+// getTemperature() 
+//-----------------------------------------------
+exports.getTemperature = function(strWhich, strScale, strType, callback) {
+  if (typeof callback !== 'function') {
+    callback = function() {};
+  }
+
+  var commands, childD, strCmdLineArgs, decimalTemperature, strReturnedTemp;
+  if (strWhich == 'gpu') {
+    strCmdLineArgs = 'measure_temp';
+    commands = [ strCmdLineArgs ];
+    childD = child_process.spawn('/opt/vc/bin/vcgencmd', commands);
+  } else {
+    strCmdLineArgs = '/sys/class/thermal/thermal_zone0/temp';
+    commands = [ strCmdLineArgs ];
+    childD = child_process.spawn('cat', commands);
+  }
+  childD.stdin.setEncoding('ascii');
+  childD.stderr.setEncoding('ascii');
+
+  childD.stdout.on('data', function(data) {
+    console.log('Spawn output: ' + data);
+    if (strWhich == 'gpu') {
+      // GPU
+      // temp=47.2'C
+      var numberPattern = /\d+\.\d+/;
+      decimalTemperature = data.toString().match(numberPattern);
+      if (strScale == 'fahrenheit') {
+        // fahrenheit
+        if (strType == 'integer') {
+          strReturnedTemp = Math.round((decimalTemperature * 9 / 5) + 32);
+        } else {
+          if (strType == 'string') {
+            strReturnedTemp = Math.round((decimalTemperature * 9 / 5) + 32).toString() + '°F';
+          } else {
+            strReturnedTemp = ((decimalTemperature * 9 / 5) + 32).toFixed(2);
+          } // else from if strType == 'string...
+        } // else from if (strType == 'int ...
+      } else {
+        // celsius
+        if (strType == 'integer') {
+          strReturnedTemp = Math.round(decimalTemperature).toString();
+        } else {
+          if (strType == 'string') {
+            strReturnedTemp = Math.round(decimalTemperature).toString() + '°C';
+          } else {
+            strReturnedTemp = decimalTemperature.toString();
+          } // end else if (strType == 'string...
+        } // end else if (strType == 'int...
+      } 
+    } else {
+      // CPU
+      decimalTemperature = data / 1000;
+      if (strScale == 'fahrenheit') {
+        if (strType == 'integer') {
+          strReturnedTemp = Math.round((decimalTemperature * 9 / 5) + 32);
+        } else {
+          if (strType == 'string') {
+            strReturnedTemp = Math.round((decimalTemperature * 9 / 5) + 32).toString() + '°F';
+          } else {
+            strReturnedTemp = ((decimalTemperature * 9 / 5) + 32).toFixed(2);
+          } // else from if strType == 'string...
+        } // else from if (strType == 'int ...
+      } else {
+        // celsius
+        if (strType == 'integer') {
+          strReturnedTemp = Math.round(decimalTemperature);
+        } else {
+          if (strType == 'string') {
+            strReturnedTemp = Math.round(decimalTemperature).toString() + '°C';
+          } else {
+            strReturnedTemp = decimalTemperature.toFixed(2);
+          } // else from if (strType == 'string...
+        } // else from if (strType == 'integer...
+      } // else from if (strScale == 'fahren...
+    } // End of if (strWhich)...
+  });
+
+  childD.stderr.on('data', function(data) {
+    console.log('Spawn error: ' + data);
+  });
+
+  childD.stdout.on('close', function(code) {
+    console.log('Spawn return: ' + code);
+    if (code == 0) {
+      callback(null, strReturnedTemp);
+    } else {
+      callback(null, 'N/A');
+    }
+  });
+  // End of getTemperature()
+}
