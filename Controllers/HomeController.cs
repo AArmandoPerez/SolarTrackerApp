b18c075b27794545852ab65a49b3bcfa551989b4
Armando <josearmandoperez84@gmail.com>
2020-03-18 20:49:45 -0600

    funciones de pososionamiento y configuracion

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Web;
 using System.Web.Mvc;
+using System.Management;
 
 namespace SolarTrackerApp.Controllers
 {
@@ -22,9 +23,35 @@ namespace SolarTrackerApp.Controllers
         }
 
 
+        public ActionResult GetCpuTemperature()
+        {
+
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(@"root\WMI", "SELECT * FROM MSAcpi_ThermalZoneTemperature");
+            Double temperature = 0;
+            String instanceName = "";
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                temperature = Convert.ToDouble(obj["CurrentTemperature"].ToString());
+                // Convert the value to celsius degrees
+                temperature = (temperature - 2732) / 10.0;
+                instanceName = obj["InstanceName"].ToString();
+            }
+
+            var jsonResult = Json(temperature, JsonRequestBehavior.AllowGet);
+            jsonResult.MaxJsonLength = int.MaxValue;
+            return jsonResult;
 
 
-       
+        }
+
+
+
+        public ActionResult Settings()
+        {
+            return View();
+        }
+
+
 
 
     }
--- Scripts/JsViews/JsDashBoard.js
+++ Scripts/JsViews/JsDashBoard.js
@@ -1,9 +1,12 @@
 ﻿
+
 $(document).ready(function () {
 
 
-    //GetRelativeHumidity();
+    GetRelativeHumidity();
     GetSolarPosition();
+    //getTemperatureCpu();
+    SolarPanelPosition();
 
     function GetSolarPosition() {
 
@@ -11,20 +14,23 @@ $(document).ready(function () {
         var sunrisePos = SunCalc.getPosition(new Date(), 23.6, -102.5)
         var sunriseAzimuth = sunrisePos.azimuth * 180 / Math.PI;
         console.log(sunriseAzimuth);
+        console.log(sunrisePos);
         console.log(times);
         $("#h3azimut").text(parseInt(sunriseAzimuth) + "°");
         $("#lastazimut").text(moment().format('h:mm:ss a'));
     }
-   
+
+
 
     function GetRelativeHumidity() {
-     
+        //oFrf80vC2oHIGV91kGPx0Db9yOZArLoh
+        //kTnd7AxjWopHBnCd7uGT6xhRGyxAVjHx
         var settings = {
             'cache': false,
             'dataType': "jsonp",
             "async": true,
             "crossDomain": true,
-            "url": "https://dataservice.accuweather.com/currentconditions/v1/242560?apikey=kTnd7AxjWopHBnCd7uGT6xhRGyxAVjHx&language=es-ES&details=true",
+            "url": "https://dataservice.accuweather.com/currentconditions/v1/242560?apikey=oFrf80vC2oHIGV91kGPx0Db9yOZArLoh&language=es-ES&details=true",
             "method": "GET",
             "headers": {
                 "accept": "application/json",
@@ -34,22 +40,77 @@ $(document).ready(function () {
 
         $.ajax(settings).done(function (response) {
 
-            $("#relativeHumedityH3").text(response[0].RelativeHumidity);
+            $("#relativeHumedityH3").text(response[0].RelativeHumidity + "%");
             $("#lastHumedity").text(moment().format('h:mm:ss a'));
-            console.log(response[0].RelativeHumidity);
-
+           
         });
 
 
     }
 
 
-    window.setInterval(function () {
-        GetRelativeHumidity();
-    }, 16000);
+    function SolarPanelPosition() {
+
+
+        /// falta implementar funcion  de seguimiento por azimut y altura asi como rastreao solar se imolemeta esto para efecto de demostracion
+       
+        var currentTime =  moment().format('H');
+        if (currentTime < 11) {
+            $("#h3panelAngule").text("20°");   
+        } else {
+
+            if (currentTime < 15) {
+                $("#h3panelAngule").text("80°"); 
+            } else {
+                $("#h3panelAngule").text("160°"); 
+            }
+        }
+ 
+
+
+
+
+
+
+    }
+
+
+    function getTemperatureCpu() {
+
+        $.ajax({
+            type: "POST",
+            url: "../Home/GetCpuTemperature",
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+
+                console.log("Request: " + XMLHttpRequest.toString() + "\n\nStatus: " + textStatus + "\n\nError: " + errorThrown);
+            },
+            success: function (result) {
+                console.log(result)
+
+
+
+            }
+        });
+
+
+    }
+
+
+
+
+    //window.setInterval(function () {
+    //    GetRelativeHumidity();
+    //}, 16000);
 
     window.setInterval(function () {
         GetSolarPosition();
+       
+    }, 25000);
+
+    window.setInterval(function () {
+        //getTemperatureCpu()
     }, 25000);
 
 });
